f092d4afe09b19887de60579c8ee2ae0d948bf37
Andrea Cremaschi <andreacremaschi@libero.it>
2011-11-17 23:15:24 +0100

    Initial Commit

---
+++ megabuffer/BDocument.h
@@ -0,0 +1,13 @@
+//
+//  BDocument.h
+//  megabuffer
+//
+//  Created by Andrea Cremaschi on 17/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BDocument : NSDocument
+
+@end
---
+++ megabuffer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  megabuffer
+//
+//  Created by Andrea Cremaschi on 17/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
