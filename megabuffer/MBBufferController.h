abb0c9ce79707bd30ad6cf22da39ef3837bd07f9
Andrea Cremaschi <andreacremaschi@libero.it>
2012-07-27 17:43:44 +0200

    aggiungo cartella Design

---
+++ megabuffer/MBBufferController.h
@@ -0,0 +1,13 @@
+//
+//  MBBufferController.h
+//  megabuffer
+//
+//  Created by Andrea Cremaschi on 20/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBBufferController : NSObject
+
+@end
---
+++ megabuffer/MBBufferController.m
@@ -0,0 +1,13 @@
+//
+//  MBBufferController.m
+//  megabuffer
+//
+//  Created by Andrea Cremaschi on 20/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MBBufferController.h"
+
+@implementation MBBufferController
+
+@end
